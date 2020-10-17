659d3186304dd9c6c73f4edadf5c0cf1137863ba
Zoe Kirsh <zoekirsh@gmail.com>
2020-10-17 03:19:42 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
   name_hash.each do |key, value|
     if value < min_value 
       min_value = value
-      min_key = key
+      min_key = key.to_s 
     end
   end
 min_key 
